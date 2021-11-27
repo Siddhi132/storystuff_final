b73e61ea60b0b830e5c37e751c3623c3300b13f4
Harshil Khamar <harshilkhamar1@gmail.com>
2021-11-27 21:19:32 +0530

    Publish Done Final

--- admin/publish_artical.php
+++ admin/publish_artical.php
@@ -5,5 +5,25 @@
     }
     $sql="UPDATE `artical` SET `delete_status` = 2 WHERE`artical_id` = ".$_POST['artical_id'];
     $result = mysqli_query($con, $sql);
-    echo 1;
+    $sql="SELECT category_id FROM artical WHERE `artical_id` = ".$_POST['artical_id'];
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_assoc($result);
+    if($row['category_id']=='STORIES')
+    {
+        echo 'story';
+    }
+    elseif ($row['category_id']=='ENTERTAINMENT') {
+        echo 'entertainment';
+    }
+    elseif ($row['category_id']=='MEMES') {
+        echo 'memes';
+    }
+    elseif ($row['category_id']=='PODCASTS')
+    {
+        echo 'podcast';
+    }
+    else
+    {
+        echo 'gallery';
+    }
 ?>
